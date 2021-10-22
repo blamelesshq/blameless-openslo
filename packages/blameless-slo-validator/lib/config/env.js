30254f0633b6c0e9fe42b65c11987a3a134fb099
Kristijan Momiroski <kristijan@blameless.com>
2021-10-22 12:42:35 +0200

    fix(load config issue resolved): fixing load config issue

--- packages/blameless-slo-validator/lib/config/env.js
+++ packages/blameless-slo-validator/lib/config/env.js
@@ -1,23 +1,27 @@
 const getBlamelessConfig = require('../utils/getBlamelessConfig')
+const isConfigSet = require('../utils/checkConfig')
 
-const config = getBlamelessConfig()
+let config
+if (isConfigSet()) {
+    config = getBlamelessConfig()
+}
 
 /**
  * @description Environment configuration
  */
 const envConfig = {
-    pat: config.REPOSITORY_PAT,
-    owner: config.REPOSITORY_OWNER,
-    repo: config.REPOSITORY_NAME,
+    pat: config?.REPOSITORY_PAT,
+    owner: config?.REPOSITORY_OWNER,
+    repo: config?.REPOSITORY_NAME,
     loginBase: process.env.BLAMELESS_OAUTH_BASE,
     clientId: process.env.BLAMELESS_OAUTH_CLIENT_ID,
     clientSecret: process.env.BLAMELESS_OAUTH_CLIENT_SECRET,
     audience: process.env.BLAMELESS_OAUTH_AUDIENCE,
     grandType: process.env.BLAMELESS_OAUTH_GRAND_TYPE,
-    getOrgIdBase: `${config.BLAMELESS_TENANT_BASE_URL}/identity/tenant`,
-    userIdBase: `${config.BLAMELESS_TENANT_BASE_URL}/identity/user/authName/`,
-    tempAuthToken: config.BLAMELESS_TEMP_AUTH_TOKEN,
-    blamelessTenantBaseUrl: `${config.BLAMELESS_TENANT_BASE_URL}/services/SLOServiceCrud/`,
+    getOrgIdBase: `${config?.BLAMELESS_TENANT_BASE_URL}/identity/tenant`,
+    userIdBase: `${config?.BLAMELESS_TENANT_BASE_URL}/identity/user/authName/`,
+    tempAuthToken: config?.BLAMELESS_TEMP_AUTH_TOKEN,
+    blamelessTenantBaseUrl: `${config?.BLAMELESS_TENANT_BASE_URL}/services/SLOServiceCrud/`,
 }
 
 module.exports = envConfig
