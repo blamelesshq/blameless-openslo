daf24bad60e9aafc2359600d7cec7f51dd8f24c7
Dillon Adams <28228453+DillonAd@users.noreply.github.com>
2022-04-13 13:30:29 -0500

    VADER-96 VADER-97 Allowing all valid Value Metric values

---
+++ packages/blameless-slo-validator/tests/lib/utils/validationResult.test.js
@@ -0,0 +1,64 @@
+const sloSchema = require('../../../lib/schema/sloSchema');
+const validationResult = require('../../../lib/utils/validationResult');
+
+describe('Validation Result', () => {
+    const validValueMetrics = [ 's', 'ms', 'rps', 'rpm', 'gb', 'mb' ];
+    it.each(validValueMetrics)('slo allows valid valueMetric "%s"', (valueMetric) => {
+        // Assemble
+        const sliDocument = {
+            apiVersion: 'blameless/v1alpha',
+            kind: 'SLO',
+            metadata: {
+                name: 'test',
+                displayName: 'test',
+                userJourney: 'test user journey'
+            },
+            spec: {
+                description: 'test description',
+                owner: 'someone@blameless.com',
+                sloStatus: 'development',
+                sliName: 'sli test',
+                target: 95.5,
+                op: 'gte',
+                value: 1,
+                valueMetric: valueMetric
+            },
+        };
+
+        // Act
+        const result = validationResult(sliDocument, sloSchema);
+
+        // Assert
+        expect(result.error).toBeFalsy();
+    });
+
+    const invalidValueMetrics = [ undefined, null, '', ' ', 'g', 'm' ];
+    it.each(invalidValueMetrics)('slo fails invalid valueMetric "%s"', (valueMetric) => {
+        // Assemble
+        const sliDocument = {
+            apiVersion: 'blameless/v1alpha',
+            kind: 'SLO',
+            metadata: {
+                name: 'test',
+                displayName: 'test',
+                userJourney: 'test user journey'
+            },
+            spec: {
+                description: 'test description',
+                owner: 'someone@blameless.com',
+                sloStatus: 'development',
+                sliName: 'sli test',
+                target: 95.5,
+                op: 'gte',
+                value: 1,
+                valueMetric: valueMetric
+            },
+        };
+
+        // Act
+        const result = validationResult(sliDocument, sloSchema);
+
+        // Assert
+        expect(result.error).toBeTruthy();
+    });
+})
